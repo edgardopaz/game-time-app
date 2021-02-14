17f1b8fdfb4b7416f5d82dcbc107daacacbd5e9d
Erik Rosales <tuf98039@temple.edu>
2021-02-14 15:01:17 -0500

    updated tweet time to 10AM

--- scheduler.py
+++ scheduler.py
@@ -5,8 +5,7 @@ from app import app
 from app.tasks import send_tweet
 
 scheduler = BackgroundScheduler(daemon=True)
-# scheduler.add_job(send_tweet, 'interval', seconds=10)
-scheduler.add_job(send_tweet, 'cron', hour=15)
+scheduler.add_job(send_tweet, 'cron', hour=10)
 scheduler.start()
 
 if __name__ == "__main__":
